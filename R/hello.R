bb5cad54be8629ab385bf21cd942f5f19a77d7bc
JeisonAlarcon <>
2021-02-19 19:05:37 -0500

    Eliminación de la función de prueba para el uso de los datos internos.

--- R/hello.R
+++
@@ -1,9 +0,0 @@
-#' Create a package skeleton
-#'
-#' @param a character: name for your new package
-#'
-#' @return Abc def, `jkm`.
-#' @export
-hello <- function(a) {
-  utils::head(Mpio_Final@data)
-}

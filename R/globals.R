f9f7b5719bd9c6459ef45e0346f0de7441b5907b
JeisonAlarcon <>
2021-06-09 20:18:04 -0500

    Primera versión de la función Agregar().

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,7 @@
 utils::globalVariables(c("Variable", "YEAR", "SEMESTRE", "Clase", "Total",
                          "Componente", "Valor", "desv", "Fecha",
                          "Code_Dept", "Code_Mun", "Departamento",
-                         ".", "Municipio", "veces"
+                         ".", "Municipio", "veces",
+                         "X", "Anos", "Periodos"
                          )
                        )

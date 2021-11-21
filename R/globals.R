28d7dc9d09274f9b7c334aa59dde305af13088e4
Jeison Mauricio Alarcón Becerra <68398794+JeisonAlarcon@users.noreply.github.com>
2021-11-21 15:28:43 -0500

    Nuevas variables globales añadidas.

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,7 @@
 utils::globalVariables(c("Variable", "YEAR", "SEMESTRE", "Clase", "Total",
-                         "Componente", "Valor", "desv", "Fecha",
+                         "Componente", "Valor", "desv", "Fecha", "sumYear",
                          "Code_Dept", "Code_Mun", "Departamento",
-                         ".", "Municipio", "veces",
-                         "X", "Anos", "Periodos"
+                         ".", "Municipio", "veces", "Estadistico",
+                         "X", "Y", "Anos", "Periodos", "Relativo"
                          )
                        )
